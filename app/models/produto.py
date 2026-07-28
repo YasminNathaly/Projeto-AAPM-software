512d2b0e7ccf3c385acbc26e047275bcb4dfc1aa
Yasmin Cavalcanti <ynathaly58@gmail.com>
2026-07-28 13:15:59 -0300

    comentar código da classe Produto

--- app/models/produto.py
+++ app/models/produto.py
@@ -1,16 +1,15 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from app.database import Base
+# from sqlalchemy import Column, Integer, String, Float
+# from app.database import Base
 
 
 
-class Produto(Base):
-    __tablename__ = "produtos"
+# class Produto(Base):
+#     __tablename__ = "produtos"
 
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(100), nullable=False)
-    categoria = Column(String(50), nullable=False)
-    cor = Column(String(50), nullable=False)
-    tamanho = Column(String(10), nullable=False)
-    preco = Column(Float, nullable=False)
-    estoque = Column(Integer, default=0, nullable=False)
+#     id = Column(Integer, primary_key=True, index=True)
+#     nome = Column(String(100), nullable=False)
+#     categoria = Column(String(50), nullable=False)
+#     cor = Column(String(50), nullable=False)
+#     tamanho = Column(String(10), nullable=False)
+#     preco = Column(Float, nullable=False)
+#     estoque = Column(Integer, default=0, nullable=False)
